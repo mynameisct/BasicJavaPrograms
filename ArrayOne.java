5dc2a41c008e254b01830ac8da767e0e74cc9c71
mynameisct <126764855+mynameisct@users.noreply.github.com>
2023-07-13 16:18:09 +0530

    Add files via upload

---
+++ ArrayOne.java
@@ -0,0 +1,19 @@
+package com.building.blocks;
+
+public class ArrayOne {
+
+	public static void main(java.lang.String[] args) {
+		
+		int array[] = new int[5];
+		array[0]= 10;
+		array[1]= 20;
+		array[2]= 30;
+		array[3]= 40;
+		array[4]= 50;
+		
+		for(int i=0; i<=4; i++)
+		{
+			System.out.println(array[i]);
+		}
+	}
+}
