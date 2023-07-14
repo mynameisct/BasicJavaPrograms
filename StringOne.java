7cba4daf5c4ed9bad8516af9156424b9f67839c3
mynameisct <126764855+mynameisct@users.noreply.github.com>
2023-07-14 11:18:29 +0530

    Add files via upload

---
+++ StringOne.java
@@ -0,0 +1,12 @@
+package com.building.blocks;
+
+public class StringOne {
+
+	public static void main(String[] args) {
+		
+		String name = "Chaitanya";
+		
+		System.out.println(name.length());
+
+	}
+}
